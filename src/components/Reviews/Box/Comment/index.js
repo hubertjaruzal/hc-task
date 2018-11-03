bdf691b9999d064ff956fb1000281104a507da07
hubertjaruzal <hubertjaruzal@gmail.com>
2018-11-03 10:11:22 +0100

    add expand after clicking functionality

--- src/components/Reviews/Box/Comment/index.js
+++ src/components/Reviews/Box/Comment/index.js
@@ -4,11 +4,32 @@ import User from './User';
 import './styles.scss';
 
 class Comment extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showFullText: false
+    }
+    
+    this.expandText = this.expandText.bind(this);
+  }
+
+  expandText() {
+    this.setState({
+      showFullDesc: true
+    });
+  }
+
   render() {
     return (
       <div className="comment">
         <span className="comment-header">Comment</span>
-        <p className="comment-text">{this.props.comment.text}</p>
+        <p
+          className={`comment-text ${this.state.showFullDesc ? 'comment-text-open' : ''}`}
+          onClick={this.expandText}
+        >
+          {this.props.comment.text}
+        </p>
         <User user={this.props.comment.user} />
       </div>
     );
--- src/components/Reviews/Box/index.js
+++ src/components/Reviews/Box/index.js
@@ -8,6 +8,22 @@ import Comment from './Comment';
 import './styles.scss';
 
 class Box extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showFullDesc: false
+    }
+    
+    this.expandDesc = this.expandDesc.bind(this);
+  }
+
+  expandDesc() {
+    this.setState({
+      showFullDesc: true
+    });
+  }
+
   render() {
     return (
       <li className="box">
@@ -18,7 +34,10 @@ class Box extends Component {
           <Thumb like={this.props.review.like}/>
           <Rate rate={this.props.review.rate}/>
         </div>
-        <p className="box-desc">
+        <p 
+          className={`box-desc ${this.state.showFullDesc ? 'box-desc-open' : ''}`}
+          onClick={this.expandDesc}
+        >
           {this.props.review.desc}
         </p>
         {
