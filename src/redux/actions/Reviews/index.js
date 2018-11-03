62432f8e1ab50fc6023a05577f284068f67eeb79
hubertjaruzal <hubertjaruzal@gmail.com>
2018-11-03 14:10:34 +0100

    store data in loclStorage

--- src/redux/actions/Reviews/index.js
+++ src/redux/actions/Reviews/index.js
@@ -1,5 +1,12 @@
 import { reviews, user } from '../../../utils/data';
 
+export const doesReviewsExists = () => {
+  if (window.localStorage && window.localStorage.hcReviews) {
+      return true;
+  }
+  return false;
+};
+
 const setReviews = (data) => ({
   type: 'SET_REVIEWS',
   data
@@ -9,7 +16,9 @@ const getReviews = (cb = () => {}) => (dispatch) => {
     setTimeout(
       () => {
         cb();
-        dispatch(setReviews(reviews));
+        dispatch(
+          setReviews(doesReviewsExists() ? JSON.parse(window.localStorage.hcReviews) : reviews)
+        );
       },
       2000
     );
@@ -24,19 +33,21 @@ const addComment = (reviewID, text, cb = () => {}) => (dispatch, getState) => {
   const reviews = getState().reviews.list;
   const review = reviews.find(item => item.id === reviewID);
 
+  const updatedReviews = Object.assign(
+    [],
+    reviews,
+    { [reviews.indexOf(review)]: Object.assign({}, review, { comment: comment }) }
+  );
+
   setTimeout(
     () => {
       cb();
-      dispatch(setReviews(
-        Object.assign(
-          [],
-          reviews,
-          { [reviews.indexOf(review)]: Object.assign({}, review, { comment: comment }) }
-        )
-      ))
+      dispatch(setReviews(updatedReviews))
     }, 
     2000
   );
+
+  window.localStorage.setItem('hcReviews', JSON.stringify(updatedReviews));
 };
 
 export {
